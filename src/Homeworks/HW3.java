95f9dfd15c2e2c70bbdf13604e4c6476aece460c
a03yevh <125909362+a03yevh@users.noreply.github.com>
2023-03-31 19:17:59 +0300

    Lessons1 + hw

---
+++ src/Homeworks/HW3.java
@@ -0,0 +1,35 @@
+package Homeworks;
+
+//Використовуючи IntelliJ IDEA, створіть проект. Створіть необхідну кількість
+//змінних типу char, кожній змінній надайте значення одного символу у форматі UNICODE. Виведіть у консоль фразу «Здрастуйте, ВАШЕ_ІМ'Я!».
+public class HW3 {
+    public static void main(String[] args) {
+        char char1 = '\u0417';
+        char char2 = '\u0434';
+        char char3 = '\u0440';
+        char char4 = '\u0430';
+        char char5 = '\u0441';
+        char char6 = '\u0442';
+        char char7 = '\u0443';
+        char char8 = '\u0439';
+        char char9 = '\u0442';
+        char char10 = '\u0435';
+
+        char char11 = '\u0020';
+
+        char char12 = '\u0404';
+        char char13 = '\u0432';
+        char char14 = '\u0433';
+        char char15 = '\u0435';
+        char char16 = '\u043D';
+        char char17 = '\u0456';
+        char char18 = '\u0439';
+
+        char char19 = '\u0021';
+
+        System.out.print("" + char1 + char2 + char3 +  char4 + char5 + char6 +
+                char7 + char8 + char9 + char10 + char11 + char12 + char13 + char14
+                + char15 + char16 + char17 + char18 + char19);
+    }
+}
+
