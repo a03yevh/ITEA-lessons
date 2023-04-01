ad9e1600696fa17a4e3484c10eed95cf7112a9f0
a03yevh <125909362+a03yevh@users.noreply.github.com>
2023-04-01 12:32:50 +0300

    HW0 update

--- src/Homeworks/HW_Lesson1/HW0.java
+++ src/Homeworks/HW_Lesson1/HW0.java
@@ -6,9 +6,20 @@ package Homeworks.HW_Lesson1;
 public class HW0 {
     public static void main(String[] args) {
         int x = 10;
-        int y = 2;
+        int y = 9;
 
-        System.out.println("/ => " + (x / y) + "\n* => " + (x * y) + "\n+ => " + (x + y) + "\n- => " + (x - y));
+        System.out.println("+ => " + (x + y));
+        System.out.println("- => " + (x - y));
+        System.out.println("* => " + (x * y));
+        System.out.println("/ => " + ((double)x / y));
+        System.out.println("% => " + (x % y));
+
+        int a = ++x;
+        int b = --y;
+        int c = x++;
+        int d = y--;
+
+        System.out.println("\na = " + a + "; b = " + b + "; c = " + c + "; d = " + d);
     }
 }
 
