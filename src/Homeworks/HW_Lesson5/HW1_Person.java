5b25d6998e81429190f6f6290b109877c6e9844d
a03yevh <125909362+a03yevh@users.noreply.github.com>
2023-05-12 23:22:27 +0300

    HW1_Matrix updated.

--- src/Homeworks/HW_Lesson5/HW1_Person.java
+++ src/Homeworks/HW_Lesson5/HW1_Person.java
@@ -4,7 +4,10 @@ public class HW1_Person {
     private String fullName;
     private int age;
 
-    HW1_Person() { }
+    HW1_Person() {
+        this.fullName = "Yulia";
+        this.age = 22;
+    }
 
     HW1_Person(String x, int y) {
         this.fullName = x;
@@ -19,12 +22,12 @@ public class HW1_Person {
     }
 
     public static void main(String[] args) {
-        HW1_Person olga = new HW1_Person();
-        HW1_Person oleg = new HW1_Person("Tarasov Oleg Nikolaevich", 33);
-        oleg.move();
-        olga.talk();
+        HW1_Person obj1 = new HW1_Person();
+        HW1_Person obj2 = new HW1_Person("Tarasov Oleg Nikolaevich", 33);
+        obj1.move();
+        obj2.talk();
 
-        olga.move();
-        oleg.talk();
+        obj2.move();
+        obj1.talk();
     }
 }
