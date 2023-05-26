2fd4ff2769c441ac6cdc3bcd33a3f286c7f46093
a03yevh <125909362+a03yevh@users.noreply.github.com>
2023-05-27 01:11:37 +0300

    Updated Main.

--- src/Homeworks/HW_Lesson6/Main.java
+++ src/Homeworks/HW_Lesson6/Main.java
@@ -65,10 +65,6 @@ public class Main {
 
         System.out.println("\n=========================================\n");
 
-        //Exercise 5:
-
-        System.out.println("\n=========================================\n");
-
         //Exercise 6:
         HW6_Printer printer = new HW6_Printer();
         printer.print("This is a regular message");
